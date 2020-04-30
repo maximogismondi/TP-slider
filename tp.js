54d245f9ba16108860c8df22269bbb6a7c0b7f7a
MaximoGismondi <63014012+MaximoGismondi@users.noreply.github.com>
2020-04-30 20:54:54 -0300

    Los separo del html en un archivo js

--- tp.js
+++ tp.js
@@ -1,1 +1,73 @@
+function animar_carrusel(milliseconds_carrusel, id_div){
 
+    var imagenActual = 0;
+    var intervalo = setInterval(cambiarImagenAdelante, milliseconds_carrusel);
+
+    function cambiarImagenAdelante(){
+    
+        clearInterval(intervalo);
+        $("#"+id_div).find('img').each(function(index){
+            if(index == imagenActual){
+                $(this).animate({marginLeft: '150%'}, 200).delay(200).fadeOut(0);     
+            }
+        });
+        
+        if (imagenActual == 3) {
+            imagenActual = 0;
+        }
+        
+        else{
+            imagenActual++;
+        }
+
+        $("#"+id_div).find('img').each(function(index) {    
+            if(index == imagenActual){
+                $(this).css({marginLeft: '-100%'}).delay(400).fadeIn(0).animate({marginLeft: '0%'},300);
+            }
+        });
+
+        intervalo = setInterval(cambiarImagenAdelante, 5000);
+    }
+
+    function cambiarImagenAtras(){
+
+        clearInterval(intervalo);
+
+        $("#"+id_div).find('img').each(function(index) {
+            if(index == imagenActual){
+                $(this).animate({marginLeft: '-100%'}, 200).delay(200).fadeOut(0);     
+            }
+        });
+
+        if (imagenActual == 0) {
+            imagenActual = 3;
+        }
+        else{
+            imagenActual--;
+        }
+
+        $("#"+id_div).find('img').each(function(index) {
+            if(index == imagenActual){
+                $(this).css({marginLeft: '150%'}).delay(400).fadeIn(0).animate({marginLeft: '0%'},300);
+            }
+        });
+
+        intervalo = setInterval(cambiarImagenAdelante, 5000);
+    }
+
+    $("#"+id_div).find('img').each(function(index) {
+        if(index!=0){
+            $(this).css("display","none");
+        }
+    });
+
+    var button1="<button id='atras'>Atras</button>"
+    var button2="<button id='adelante'>Adelante</button>"
+    var textoImagenActual="<label>Imagen actual: </label>"
+
+    $("#"+id_div).after(button1,button2, textoImagenActual);
+
+    $("#atras").click(cambiarImagenAtras);
+    $("#adelante").click(cambiarImagenAdelante);
+
+}
