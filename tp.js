7fe4b2d78d2e056560ac1cb6796452516348be11
DamianBlum <62249397+DamianBlum@users.noreply.github.com>
2020-05-03 16:07:47 -0300

    Avance en bolitas

--- tp.js
+++ tp.js
@@ -12,6 +12,8 @@ function animar_carrusel(milliseconds_carrusel, id_div){
             }
         });
 
+        $("#circulo"+(imagenActual+1)).css('background','white');
+
         if (imagenActual == cantImagenes) {
             imagenActual = 0;
         }
@@ -27,6 +29,8 @@ function animar_carrusel(milliseconds_carrusel, id_div){
             }
         });
 
+        $("#circulo"+(imagenActual+1)).css('background','red');
+
         intervalo = setInterval(cambiarImagenAdelante, 5000);
     }
 
@@ -39,6 +43,8 @@ function animar_carrusel(milliseconds_carrusel, id_div){
                 $(this).animate({marginLeft: '-75%'}, 200).delay(200).fadeOut(0);     
             }
         });
+
+        $("#circulo"+(imagenActual+1)).css('background','white');
 
         if (imagenActual == 0) {
             imagenActual = cantImagenes;
@@ -54,6 +60,8 @@ function animar_carrusel(milliseconds_carrusel, id_div){
             }
         });
 
+        $("#circulo"+(imagenActual+1)).css('background','red');
+
         intervalo = setInterval(cambiarImagenAdelante, 5000);
     }
 
@@ -63,14 +71,15 @@ function animar_carrusel(milliseconds_carrusel, id_div){
         }
     });
 
-    var conjuntoCirculos="<div id=barraDeCirculosYBotones display: inline>"+
+    var barraDeCirculosYBotones="<div class='barraDeCirculosYBotones'>"+
     "<button id='atras'><</button>"+
-    "<div id='circulo1' style='background: green;width:1%;height:1%; border-radius:50%;border: solid  green;float: left';></div>"+
-    "<div id='circulo2' style='background: black;width:1%;height:1%; border-radius:50%;border: solid  black;float: left';></div>"+
-    "<div id='circulo3' style='background: black;width:1%;height:1%; border-radius:50%;border: solid  black;float: left';></div>"+
-    "<div id='circulo4' style='background: black;width:1%;height:1%; border-radius:50%;border: solid  black;float: left';></div>"+
+    "<div id='circulo1' class='circulo'></div>"+
+    "<div id='circulo2' class='circulo'></div>"+
+    "<div id='circulo3' class='circulo'></div>"+
+    "<div id='circulo4' class='circulo'></div>"+
     "<button id='adelante'>></button>"+
     "</div>";
+
 
     var cantImagenes = 0;
 
@@ -80,18 +89,16 @@ function animar_carrusel(milliseconds_carrusel, id_div){
         }
     });    
     $("#"+id_div).after(barraDeCirculosYBotones);
-    //document.getElementById('id').className = **; //Para cambiarle la clase a un circulo
-    $("#"+id_div).css({marginLeft: '16%'});
-    //$("#atras").css({marginLeft: '23%'});
-    //$("#ciculo1").css({marginLeft: '22.2%'});
-    //$("#ciculo2").css({marginLeft: '0.7%'});
-    //$("#ciculo3").css({marginLeft: '0.7%'});
-    //$("#ciculo4").css({marginLeft: '0.7%'});
-    //("#adelante").css({marginLeft: '22.2%'});
+    $("#circulo"+(imagenActual+1)).css('background','red');
+    $("#"+id_div).css({marginLeft: '26%'});
+    $("#atras").css({marginLeft: '26.5%'});
+    $("#circulo1").css({marginLeft: '20%'});
+    $("#circulo2").css({marginLeft: '0.7%'});
+    $("#circulo3").css({marginLeft: '0.7%'});
+    $("#circulo4").css({marginLeft: '0.7%'});
+    $("#adelante").css({marginLeft: '20%'});
 
-    //$("#"+id_div).prepend(button1);
-    //$("#"+id_div).append(button2);
-    //$("#"+id_div).after(textoImagenActual,NumeroImagenActual);
+    //document.getElementById('id').className = **; //Para cambiarle la clase a un circulo
 
     $("#atras").click(cambiarImagenAtras);
     $("#adelante").click(cambiarImagenAdelante);
